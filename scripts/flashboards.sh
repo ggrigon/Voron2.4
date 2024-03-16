3eeda2390df8da4869fd934f9a5f2016d87c09f8
Guilherme Rigon <guilherme@rigon.info>
2024-03-15 21:39:42 -0300

    telegram bot

---
+++ scripts/flashboards.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd ~/klipper
+
+# linux flash
+make clean KCONFIG_CONFIG=config.linux
+#make menuconfig KCONFIG_CONFIG=config.linux
+make KCONFIG_CONFIG=config.linux
+make flash
+
+# spider 2.3 
+make clean KCONFIG_CONFIG=config.octopus
+make KCONFIG_CONFIG=config.octopus
+make flash FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_lpc1768_08A0000B610039AF9AF4F45BC02000F5-if00
+
+# sht36
+
+# /opt/klipper-venv/bin/python /opt/CanBoot/scripts/flash_can.py -i can0 -f out/klipper.bin -u 11ab3fae057c
+# /opt/klipper-venv/bin/python /opt/klipper/scripts/canbus_query.py can0
